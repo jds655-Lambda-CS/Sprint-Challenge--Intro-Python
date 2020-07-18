3319bafbf3e0b672385f791443e1ac45064da71a
Joshua Sharp <sharp.josh@gmail.com>
2020-07-18 11:32:59 -0400

    cityreader written and tested.  No stretch yet

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,17 @@
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f'Name: {self.name}  Lat: {self.lat}  Lon: {self.lon}'
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -16,19 +28,30 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # Ensure that the lat and lon valuse are all floats
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
-    return cities
+    # TODO Implement the functionality to read from the 'cities.csv' file
+    # Ensure that the lat and lon valuse are all floats
+    # For each city record, create a new City instance and add it to the
+    # `cities` list
+    # city,state_name,county_name,lat,lng,population,density,timezone,zips
+    inputfile = './cities.csv'
+    with open(inputfile, 'r') as infile:
+        reader = csv.reader(infile, delimiter=',')
+        linenum = 1
+        for row in reader:
+            if linenum != 1:
+                cities.append(City(row[0], row[3], row[4]))
+            linenum += 1
+        return cities
+
 
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
     print(c)
+
 
 # STRETCH GOAL!
 #
@@ -62,10 +85,10 @@ for c in cities:
 # TODO Get latitude and longitude values from the user
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-  # within will hold the cities that fall within the specified region
-  within = []
-  
-  # Go through each city and check to see if it falls within 
-  # the specified coordinates.
+    # within will hold the cities that fall within the specified region
+    within = []
 
-  return within
+    # Go through each city and check to see if it falls within
+    # the specified coordinates.
+
+    return within
